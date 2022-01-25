30d3f607e42765b1bc47c1ac93b8fec0683e3862
Ravi Panchumarthy <ravi.panchumarthy@intel.com>
2022-01-25 10:05:53 -0500

    Create export-to-ov.sh

---
+++ examples/tcga/savedmodels/export-to-ov.sh
@@ -0,0 +1,7 @@
+
+mo  --input "net/inp_x_test,net/inp_x_sub_0_test" \
+--input_shape [10,1,45,45,45],[10,1,26,26,26] \
+--input_meta_graph deepmedic-4-ov.model.ckpt.meta \
+--output net/transpose_138 \
+--output_dir fp32 \
+--disable_nhwc_to_nchw
